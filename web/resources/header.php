aa5a20824578f0bd78ae0d902dcd2dd9577600ec
javiercoulo <javiercoulon@gmail.com>
2016-03-10 10:58:56 -0330

    little fix done

--- web/resources/header.php
+++ web/resources/header.php
@@ -1,11 +1,4 @@
-<?php
-	require_once "H:/My Documents/Desktop/3715/git/web/resources/constants.php";
 
-
-
-?>
-
-		
 		
 <link rel="stylesheet" type="text/css" href="<?php echo $_CSSURL;?>header.css">
 <div id="header_container">
